726223b84eda9a980205ca2cd6729ed3a9391365
uchel09 <ucelrussel77@gmail.com>
2026-01-15 23:57:23 +0800

    update

--- lib/app/providers/api_provider.dart
+++ lib/app/providers/api_provider.dart
@@ -55,3 +55,24 @@ class ApiProvider {
           ),
         );
 }
+
+// kalau kode diatas error pakai ini kembali 
+
+
+// import 'package:dio/dio.dart';
+// import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+// import 'package:cookie_jar/cookie_jar.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// class ApiProvider {
+//   static final Dio dio = Dio(
+//     BaseOptions(
+//       baseUrl: dotenv.env['API_BASE_URL'] ?? 'http://localhost:8080/api',
+//       connectTimeout: const Duration(seconds: 15),
+//       receiveTimeout: const Duration(seconds: 15),
+//       contentType: 'application/json',
+//     ),
+//   )..interceptors.add(CookieManager(CookieJar()));
+//   
+// } 
+
